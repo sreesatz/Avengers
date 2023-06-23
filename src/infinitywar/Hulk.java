322e712e62bbdb6be023258622999320c7c38dc7
sreesatz <raju.sathish@gmail.com>
2023-06-23 11:25:43 +0000

    Initial commit

---
+++ src/infinitywar/Hulk.java
@@ -0,0 +1,21 @@
+package infinitywar;
+
+public class Hulk {
+
+	public static void main(String[] args) {
+		System.out.println("SHOULD I CHANGE MY JOB?");
+
+		double currentSalary = 100000;
+		double offeredSalary = 1000000;
+		
+		if (offeredSalary > currentSalary) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+		
+		
+		
+	}
+	
+}
---
+++ src/infinitywar/Ironman.java
@@ -0,0 +1,20 @@
+package infinitywar;
+
+public class Ironman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+int[] marks = { 99, 87, 92, 95, 98 };
+		
+		System.out.println("Chemistry");
+		System.out.println(marks[2]); // 3rd element - 92
+		System.out.println("Maths");
+		System.out.println(marks[0]); // 1st element - 99
+		System.out.println("All subjects");
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(marks[i]);
+		}
+	}
+}
